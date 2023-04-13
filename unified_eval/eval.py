1b4727c9dde2bfb04d86c79db4f2af5e7d5e957a
Bryan Cai <bryan.x.cai@gmail.com>
2023-04-13 00:33:06 -0700

    added psytar

--- unified_eval/eval.py
+++ unified_eval/eval.py
@@ -130,9 +130,9 @@ def get_biogpt_embed(phrase_list, model, tokenizer, device, show_progress=False,
             input_gpu_0 = torch.LongTensor(input_ids[now_count:min(
                 now_count + batch_size, count)]).to(device)
             if summary_method == "CLS":
-                embed = model(input_gpu_0, output_hidden_states=True).hidden_states[0][:,1,:]
+                embed = model(input_gpu_0, output_hidden_states=True).hidden_states[-1][:,0,:]
             if summary_method == "MEAN":
-                embed = torch.mean(model(input_gpu_0, output_hidden_states=True).hidden_states[0], dim=1)
+                embed = torch.mean(model(input_gpu_0, output_hidden_states=True).hidden_states[-1], dim=1)
             if normalize:
                 embed_norm = torch.norm(
                     embed, p=2, dim=1, keepdim=True).clamp(min=1e-12)
@@ -216,7 +216,7 @@ def get_distilbert_embed(phrase_list, model, tokenizer, device, show_progress=Fa
             input_gpu_0 = torch.LongTensor(input_ids[now_count:min(
                 now_count + batch_size, count)]).to(device)
             if summary_method == "CLS":
-                embed = model(input_gpu_0).last_hidden_state[:,1,:]
+                embed = model(input_gpu_0).last_hidden_state[:,0,:]
             if summary_method == "MEAN":
                 embed = torch.mean(model(input_gpu_0).last_hidden_state, dim=1)
             if normalize:
