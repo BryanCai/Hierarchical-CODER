386f262ab76e77a1e1e7f3517f9f56be35829ac3
Bryan Cai <bryan.x.cai@gmail.com>
2023-04-18 14:15:52 -0400

    minor

--- unified_eval/print_json.py
+++ unified_eval/print_json.py
@@ -68,17 +68,17 @@ def run(file):
     for i in [
               "cadec",
               ]:
-        print(data[i])
+        print(data[i][0])
+        print(data[i][1])
 
     print("\n")
 
     for i in [
+              "example0_cos_sim",
               "example1_cos_sim",
               "example2_cos_sim",
               "example3_cos_sim",
               "example4_cos_sim",
-              "example5_cos_sim",
-              "example6_cos_sim",
               ]:
         print(data[i])
 
@@ -87,15 +87,17 @@ def run(file):
 if __name__ == '__main__':
     output_path_list = [
                         "/home/tc24/BryanWork/saved_models/output_coder_base/output2_300000.json",
-                        "/home/tc24/BryanWork/saved_models/output_unified_ms/output2_300000.json",
+#                        "/home/tc24/BryanWork/saved_models/output_unified_ms/output2_300000.json",
                         "/home/tc24/BryanWork/saved_models/old/output_unified_3/output2_300000.json",
                         "/home/tc24/BryanWork/saved_models/old/output_unified_ft_5/output2_20000.json",
-                        "/home/tc24/BryanWork/saved_models/output_unified_ft_7/output2_10000.json",
+#                        "/home/tc24/BryanWork/saved_models/output_unified_ft_7/output2_10000.json",
                         "/home/tc24/BryanWork/saved_models/output_unified_ft_8/output2_10000.json",
-                        "/home/tc24/BryanWork/saved_models/output_unified_ft_9/output2_10000.json",
+#                        "/home/tc24/BryanWork/saved_models/output_unified_ft_9/output2_10000.json",
                         "/home/tc24/BryanWork/CODER/unified_eval/fixed_model_eval/sapbert.json",
                         "/home/tc24/BryanWork/CODER/unified_eval/fixed_model_eval/coder.json",
                         "/home/tc24/BryanWork/CODER/unified_eval/fixed_model_eval/biobert1_1.json",
+                        "/home/tc24/BryanWork/CODER/unified_eval/fixed_model_eval/biogpt.json",
+                        "/home/tc24/BryanWork/CODER/unified_eval/fixed_model_eval/distilbert.json",
                         ]
 
     for i in output_path_list:
